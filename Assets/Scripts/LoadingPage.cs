6706a9ad82fcebb82e404428675a8db6d5aa64da
Adam Din <64247407+AdamDN29@users.noreply.github.com>
2022-04-21 09:02:12 +0700

    Add Scene

---
+++ Assets/Scripts/LoadingPage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingPage : MonoBehaviour
+{
+    public static int loadingTime = 0;
+    public static int waitTime = 3;
+
+    
+     // Update is called once per frame
+    void Update()
+    {
+        if (waitTime == 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+        else
+        {
+            //loadingTime += 1 * Time.deltaTime;
+            StartCoroutine(TickTack());
+        }
+    }
+
+    IEnumerator TickTack()
+    {
+        yield return new WaitForSeconds(1);
+        waitTime -= 1;
+
+    }
+}
